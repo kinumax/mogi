16eb3efc2c143bfbc95502178d4b80927cb1fc29
kinumax <hidekiiinuma753@gmail.com>
2025-03-20 21:32:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,37 @@ startButton.addEventListener('click', () => {
 function gameLoop() {
     setInterval(createFruit, 1000); // 1秒ごとにフルーツ追加
 }
+
+// プレイヤーの描画関数を修正
+Player.prototype.draw = function() {
+    if (IMAGES.player && IMAGES.player.ninja) {
+        // 画像を使用して描画
+        ctx.drawImage(
+            IMAGES.player.ninja,
+            this.x - this.width / 2,
+            this.y - this.height,
+            this.width,
+            this.height
+        );
+    } else {
+        // フォールバック（画像がない場合）
+        ctx.fillStyle = '#FF4081';
+        ctx.fillRect(this.x - this.width / 2, this.y - this.height, this.width, this.height);
+    }
+};
+
+// 障害物の描画関数を修正
+Obstacle.prototype.draw = function() {
+    if (IMAGES.obstacles && IMAGES.obstacles.torii) {
+        ctx.drawImage(
+            IMAGES.obstacles.torii,
+            this.x - this.width / 2,
+            this.y - this.height,
+            this.width,
+            this.height
+        );
+    } else {
+        ctx.fillStyle = '#8B4513';
+        ctx.fillRect(this.x - this.width / 2, this.y - this.height, this.width, this.height);
+    }
+};
